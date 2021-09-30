d0f8c75a73aae8077213d80e9a01693a3432bfd5
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2021-09-30 14:45:54 -0600

    main component rendered

---
+++ client/client.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react";
+
+const App = () => <div>This is a React Component</div>;
+
+ReactDOM.render(
+  <App />,
+  document.querySelector("#container")
+  );
--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
+import { readFileSync } from "fs";
 
 const app = new express();
 
-app.get("/", (_req, res) => {
-  res.send(`<h1>React App is Working!</h1>`);
+app.use(express.static("dist"));
+
+
+app.get("/", async (_req, res) => {
+  
+    const index = readFileSync('public/index.html', 'utf8');
+    res.send(index);
+
+  // res.send(`<h1>React App is Working!</h1>`);
 });
+
 
 app.listen(4000);
 console.log("Server is Running");
